7206039f9f9ad3d2f3acf1610195238f06012933
AnnaPlis1 <112053615+AnnaPlis1@users.noreply.github.com>
2023-01-24 10:20:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,23 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const point = '10';
+    const hyphen = '11';
+    const space = '**********';
+    const code = expr.split(space);
+    const answer = code.map((item) => { 
+        let arr = [];
+        for (let i = 0; i < item.length; i += 10) {
+        const result = item
+        .slice(i, i + 10)
+        .replace(/10/g, '.')
+        .replace(/11/g, '-')
+        .replace(/00/g, '');
+        arr.push(MORSE_TABLE[result]);
+    }
+    return arr.join('');
+});
+return answer.join(' ');
 }
 
 module.exports = {
